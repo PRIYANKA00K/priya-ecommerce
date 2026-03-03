d50fcb5baca98f4f8736af74e795f37c881a8ac1
PRIYANKA R <pr989859@gmail.com>
2026-03-03 22:10:14 +0530

    Add files via upload
    Defines the User schema with fields for email, name, password, Google ID, OTP, verification status, etc.

---
+++ User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  mobile: String,
+  password: String,
+  googleId: String,
+  otp: String,
+  otpExpiry: Date,
+  isVerified: { type: Boolean, default: false }
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
